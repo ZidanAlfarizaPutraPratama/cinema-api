62e8964505ab0e9edaa5522b8e1a89917d85d244
ZidanAlfarizaPutraPratama <zidanalfariza@gmail.com>
2023-10-31 16:23:03 +0700

    perubahan terakhir code,telah selesai

--- src/controller/reportController.js
+++ src/controller/reportController.js
@@ -81,7 +81,6 @@ const getTicketReportsByMovies = async (req, res) => {
     res.status(500).json({ error: 'Failed to retrieve ticket reports by movies' });
   }
 };
-
 const getTicketReportsByGenres = async (req, res) => {
   try {
     const { start_date, end_date } = req.query;
@@ -89,46 +88,18 @@ const getTicketReportsByGenres = async (req, res) => {
     if (!start_date || !end_date) {
       return res.status(400).json({ error: 'Both start_date and end_date are required.' });
     }
-    const pipeline = [
-      {
-        $lookup: {
-          from: 'Movies',
-          localField: 'movie_id',
-          foreignField: 'movie_id',
-          as: 'movie',
-        },
+
+    const dateFilter = {
+      date: {
+        $gte: new Date(start_date),
+        $lte: new Date(end_date),
       },
-      {
-        $unwind: '$movie',
-      },
-      {
-        $unwind: '$genres',
-      },
-      {
-        $match: {
-          date: {
-            $gte: new Date(start_date), 
-            $lte: new Date(end_date), 
-          },
-          is_used: true, // Hanya tiket yang digunakan
-        },
-      },
-      {
-        $group: {
-          _id: '$genre.name', 
-          totalTickets: { $sum: 1 }, // Menghitung jumlah tiket
-        },
-      },
-    ]; 
+      is_used: true,
+    };
+
     const pipeline2 = [
       {
-        $match: {
-          date: {
-            $gte: start_date, 
-            $lte: end_date, 
-          },
-          is_used: true,
-        },
+        $match: dateFilter,
       },
       {
         $lookup: {
@@ -136,116 +107,85 @@ const getTicketReportsByGenres = async (req, res) => {
           localField: "no_publish",
           foreignField: "no_publish",
           as: "moviePublish"
-        }
+        },
       },
-      {
-        $unwind: "$moviePublish"
-      },
-      {
-        $lookup: {
-          from: "movies",
-          localField: "moviePublish.movie_id",
-          foreignField: "movie_id",
-          as: "movie"
-        }
-      },
-      {
-        $unwind: "$movie"
-      },
-      {
-        $unwind: "$movie.genres"
-      },
-      {
-        $lookup: {
-          from: "genres",
-          localField: "movie.genres",
-          foreignField: "genre_id",
-          as: "genre"
-        }
-      },
-      {
-        $unwind: "$genre"
-      },
-      {
-        $group: {
-          _id: "$genre.name",
-          nama_genre: {$first: "$genre.name"},
-          totalTicket: {$sum: 1}
-        }
-      }
-      
-    ]   
+    ];
 
     const ticketReportsByGenres = await Ticket.aggregate(pipeline2);
 
+    console.log(ticketReportsByGenres);
 
-    console.log(ticketReportsByGenres)
     if (ticketReportsByGenres.length === 0) {
-      console.log('No data found.'); 
+      console.log('No data found.');
       return res.status(404).json({ error: 'No sold tickets found in the specified date range.' });
     }
 
-    console.log('Ticket Reports:', ticketReportsByGenres); 
+    console.log('Ticket Reports:', ticketReportsByGenres);
     res.status(200).json({ message: 'Ticket reports by genres retrieved successfully', ticketReportsByGenres });
   } catch (error) {
-    console.log('Error:', error); 
+    console.log('Error:', error);
     res.status(500).json({ error: 'Failed to retrieve ticket reports by genres' });
   }
 };
 
 const getMoviePublishReportsWithSeats = async (req, res) => {
   try {
-    const { seat_status, start_date, end_date } = req.query;
+    const seatStatus = req.query.seat_status;
+    const startDate = req.query.start_date;
+    const endDate = req.query.end_date;
 
-    if (!seat_status || !start_date || !end_date) {
-      return res.status(400).json({ error: 'seat_status, start_date, and end_date are required.' });
-    }
-
-    const startDate = new Date(start_date);
-    const endDate = new Date(end_date);
-    endDate.setHours(23);
-    endDate.setMinutes(59);
-    endDate.setSeconds(59);
-
-    const matchStage = {
-      $match: {
-        start_time: { $gte: startDate, $lte: endDate },
+    const dateFilter = {
+      start_time: {
+        $gte: new Date(startDate),
+        $lte: new Date(endDate),
       },
     };
 
-    if (seat_status === 'full') {
-      matchStage.$match.available_seats = 0;
-    } else if (seat_status === 'leftover') {
-      matchStage.$match.available_seats = { $gt: 0 };
+    if (seatStatus === 'full') {
+      dateFilter.existed_seats = 0;
     }
 
-    const groupStage = {
-      $group: {
-        _id: '$movie_id',
-        totalPublishes: { $sum: 1 },
-        totalSeats: { $sum: '$available_seats' },
+    console.log('Date Filter:', dateFilter);
+
+    // Aggregation
+    const pipeline = [
+      {
+        $lookup: {
+          from: "movies",
+          localField: "movie_id",
+          foreignField: "movie_id",
+          as: "movie_lookup"
+        }
       },
-    };
+      {
+        $unwind: "$movie_lookup"
+      },
+      {
+        $addFields: {
+          existed_seats: {$size: "$available_seat"}
+        }
+      },
+      {
+        $match: dateFilter,
+      },
+    ];
 
-    const sortStage = {
-      $sort: { _id: 1 },
-    };
+    const result = await MoviePublish.aggregate(pipeline);
 
-    const pipeline = [matchStage, groupStage, sortStage];
-
-    const moviePublishReports = await MoviePublish.aggregate(pipeline);
-
-    if (moviePublishReports.length === 0) {
-      return res.status(404).json({ error: 'No movie publishes found in the specified date range and seat condition.' });
+    console.log('Result:', result);
+    let message = '';
+    if (seatStatus === 'full') {
+      message = 'Ruangan sudah habis.';
+    } else {
+      message = 'Masih ada sisa kursi yang tersedia.';
     }
 
-    res.status(200).json({ message: 'Movie publish reports with seat condition retrieved successfully', moviePublishReports });
+    res.json({ message, result });
   } catch (error) {
-    console.log('Error:', error);
-    res.status(500).json({ error: 'Failed to retrieve movie publish reports with seat condition' });
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Terjadi kesalahan dalam permintaan.' });
   }
 };
-
 
 module.exports = {
   getTicketReports,
